c9cb11070c12af97f08cea4dd69a938111c71222
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-11 16:32:24 +0100

    autenthication

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
