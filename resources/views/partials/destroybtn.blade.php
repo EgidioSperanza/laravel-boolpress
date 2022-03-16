a63f8fcfe28e00bd62f3e9faf9d47dde90c5856c
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-16 17:10:49 +0100

    fix background delete modal

--- resources/views/partials/destroybtn.blade.php
+++ resources/views/partials/destroybtn.blade.php
@@ -7,7 +7,7 @@
   <!-- Modal -->
   <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <div class="modal-content bg-dark text-light">
         <div class="modal-header">
           <h5 class="modal-title" id="staticBackdropLabel">Conferma Cancellazione Dati</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
