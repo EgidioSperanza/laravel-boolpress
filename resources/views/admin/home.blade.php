0ef9849c1453f80709140d1e479d5f1e1f83e1c4
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-17 21:22:49 +0100

    green dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card  bg-success lead">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
